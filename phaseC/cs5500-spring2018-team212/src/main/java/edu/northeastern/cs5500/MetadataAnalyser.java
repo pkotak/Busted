0ef7290fc518b66ddf3f2a1c4fb1c3cf37e755c6
abhiruchi <karwa.a@husky.neu.edu>
2018-03-22 23:15:12 -0400

    CS212-9 #close added strategy 2 (#18)
    * removing git errors
    
    * Sample results added
    
    * Comparison starategy 2
    
    * test commented
    
    * fixed for Sonarqube
    
    * person tests added
    
    * minor changes
    
    * minor changes
    
    * tests updated
    
    * metadata final

--- phaseC/cs5500-spring2018-team212/src/main/java/edu/northeastern/cs5500/MetadataAnalyser.java
+++ phaseC/cs5500-spring2018-team212/src/main/java/edu/northeastern/cs5500/MetadataAnalyser.java
@@ -7,11 +7,22 @@ import java.nio.file.Path;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * class to check the metadata for the assignments
+ * @author abhiruchi
+ *
+ */
+
 public class MetadataAnalyser {
 
 	private static final Logger LOGGER = Logger.getLogger(CallLibrary.class.getName());
 
 
+	/**
+	 * method to extract metadata
+	 * @param filepath of the given file
+	 * @return the metadata as string
+	 */
 	public String getMetaData(String filepath) {
 		Path path = FileSystems.getDefault().getPath(filepath);
 		StringBuilder jsonString = new StringBuilder();
@@ -23,6 +34,12 @@ public class MetadataAnalyser {
 		return jsonString.toString();
 	}
 	
+	/**
+	 * method to compare metadata
+	 * @param f1path - path of first file
+	 * @param f2path - path of second file
+	 * @return int indicating comparison files
+	 */
 	public int checkifSame(String f1path, String f2path) {
 		String f1md = getMetaData(f1path);
 		String f2md = getMetaData(f2path);
